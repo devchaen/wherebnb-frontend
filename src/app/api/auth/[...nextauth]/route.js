d5892cd660bb01ed9cb8d7f1c88e1e362c0802ed
devchaen <cogus1221@gmail.com>
2024-01-11 03:12:47 +0900

    WHEREBNB-29 feat/next-auth 기본 config

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,71 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Wherebnb",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid credentials");
+        }
+
+        // 여기에서 server api와 통신
+
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
+        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+        // You can also use the `req` object to obtain additional parameters
+        // (i.e., the request IP address)
+        const res = await fetch("http://localhost:8080/user/login", {
+          method: "POST",
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" },
+        });
+        const user = await res.json();
+
+        // If no error and we have user data, return it
+        if (res.ok && user) {
+          return user;
+        }
+        // Return null if user data could not be retrieved
+        return null;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token, user }) {
+      session.user = token;
+      return session;
+    },
+  },
+  debug: process.env.NODE_ENV === "development",
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,6 +4,7 @@ import Navbar from "@/app/components/navbar/Navbar";
 import LoginModal from "@/app/components/ui/modals/LoginModal";
 import RegisterModal from "@/app/components/ui/modals/RegisterModal";
 import ToastProvider from "@/app/providers/ToastProvider";
+import { SessionProvider } from "next-auth/react";
 
 export const metadata = {
   title: "WhereBnb",
@@ -14,15 +15,17 @@ const font = Nunito({
   subsets: ["latin"],
 });
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children, session }) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <ToastProvider />
-        <RegisterModal />
-        <LoginModal />
-        <Navbar />
-        <div className="md:pt-[200px] pt-[95px]">{children}</div>
+        <SessionProvider session={session}>
+          <ToastProvider />
+          <RegisterModal />
+          <LoginModal />
+          <Navbar />
+          <div className="md:pt-[200px] pt-[95px]">{children}</div>
+        </SessionProvider>
       </body>
     </html>
   );
